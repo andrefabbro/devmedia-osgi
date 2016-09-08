a25b8e1eb067f2b367fb98a2efd70d553ebf45c3
André Fabbro <andre.fabbro@liferay.com>
2016-09-08 16:48:50 -0300

    Adds the referente to the exported service

--- devmedia-osgi-exemplo/src/main/java/br/com/devmedia/osgi/exemplo/Activator.java
+++ devmedia-osgi-exemplo/src/main/java/br/com/devmedia/osgi/exemplo/Activator.java
@@ -2,16 +2,26 @@ package br.com.devmedia.osgi.exemplo;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import br.com.devmedia.hello.service.HelloService;
 
 public class Activator implements BundleActivator {
 
+    private ServiceReference helloServiceReference;
+
     @Override
     public void start(BundleContext context) throws Exception {
-        System.out.println("Hello world");
+        helloServiceReference =
+            context.getServiceReference(HelloService.class.getName());
+        HelloService helloService =
+            (HelloService) context.getService(helloServiceReference);
+        System.out.println(helloService.sayHello());
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         System.out.println("Goodbye World");
+        context.ungetService(helloServiceReference);
     }
 }
